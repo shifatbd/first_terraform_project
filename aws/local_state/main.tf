24e06b484dd49d5939f333bfb131fd539bf08496
Md Shihab Hossain Shifat <sh.shifatbd@gmail.com>
2025-09-30 12:18:34 +0600

    Update main.tf

--- aws/local_state/main.tf
+++ aws/local_state/main.tf
@@ -5,7 +5,6 @@ terraform {
       version = "~> 6.0"
     }
   }
-}
 
   required_version = ">= 1.2.0"
 }
@@ -15,12 +14,22 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# Look up the latest Debian 12 AMI
+data "aws_ami" "debian12" {
+  most_recent = true
+  owners      = ["136693071363"] # Official Debian account on AWS Marketplace
+
+  filter {
+    name   = "name"
+    values = ["debian-12-amd64-*"]
+  }
+}
+
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = data.aws_ami.debian12.id
   instance_type = "t2.micro"
 
   tags = {
     Name = "Terraform_Demo"
   }
 }
-
